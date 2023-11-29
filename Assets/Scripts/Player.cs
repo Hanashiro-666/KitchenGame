ecd1e6ff888420cb2df08a469e80b282f0d01965
Hanashiro-666 <yascuo@gmail.com>
2023-11-29 17:25:57 +0200

    Commit 6.5(16:00 - 17:00)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,6 +16,31 @@ public class Player : MonoBehaviour
 
    private bool isWalking;
 
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {   
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                //Has ClearCouner 
+                clearCounter.Interact();
+            }
+        }
+    }
     private void Update()
     {
        HandleMovement();
@@ -44,7 +69,6 @@ public class Player : MonoBehaviour
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
             {
                 //Has ClearCouner 
-                clearCounter.Interact();
             }
         }
     }
